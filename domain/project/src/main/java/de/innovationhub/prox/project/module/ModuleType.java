2e1ac3901580426b8b41278cb09e89fb6c2f81b3
Tobias Hund <tobias.hund@innovation-hub.de>
2022-10-15 12:55:11 +0200

    add filter method for modules based on disciplines

--- domain/project/src/main/java/de/innovationhub/prox/project/module/ModuleType.java
+++ domain/project/src/main/java/de/innovationhub/prox/project/module/ModuleType.java
@@ -1,5 +1,6 @@
 package de.innovationhub.prox.project.module;
 
+import java.util.List;
 import java.util.UUID;
 import lombok.Data;
 
@@ -9,9 +10,14 @@ public class ModuleType {
   private final String key;
   private final String name;
 
-  public ModuleType(UUID id, String key, String name) {
+  private final List<String> disciplines;
+
+  private boolean active = true;
+
+  public ModuleType(UUID id, String key, String name, List<String> disciplines) {
     this.id = id;
     this.key = key;
     this.name = name;
+    this.disciplines = disciplines;
   }
 }
--- domain/project/src/main/java/de/innovationhub/prox/project/module/ModuleTypePort.java
+++ domain/project/src/main/java/de/innovationhub/prox/project/module/ModuleTypePort.java
@@ -1,5 +1,6 @@
 package de.innovationhub.prox.project.module;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -8,4 +9,5 @@ public interface ModuleTypePort {
   Optional<ModuleType> getById(UUID id);
   Optional<ModuleType> getByKey(String key);
   List<ModuleType> getAll();
+  List<ModuleType> getByDisciplines(Collection<String> disciplineKeys);
 }

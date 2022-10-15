2ec284121d832f2d980e49705c27251a22cd1265
Tobias Hund <tobias.hund@innovation-hub.de>
2022-10-15 11:40:55 +0200

    add repository method

--- domain/tag/src/main/java/de/innovationhub/prox/tag/TagPort.java
+++ domain/tag/src/main/java/de/innovationhub/prox/tag/TagPort.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 public interface TagPort {
   Tag save(Tag tag);
   Optional<Tag> getByTag(String tag);
+  List<Tag> getOrCreateTags(Collection<String> tags);
   boolean existsByTag(String tag);
   List<Tag> findMatching(String tag);
   List<Tag> findCommon(Collection<String> tags);

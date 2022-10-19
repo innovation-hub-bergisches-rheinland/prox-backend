7fff75e3434925bcf227b53f6a221f75ea607116
Tobias Hund <tobias.hund@innovation-hub.de>
2022-10-19 13:44:43 +0200

    remove event publisher from usecase

--- src/main/java/de/innovationhub/prox/modules/profile/application/user/usecase/RegisterUserUseCaseHandler.java
+++ src/main/java/de/innovationhub/prox/modules/profile/application/user/usecase/RegisterUserUseCaseHandler.java
@@ -1,11 +1,9 @@
 package de.innovationhub.prox.modules.profile.application.user.usecase;
 
 import de.innovationhub.prox.modules.commons.application.ApplicationComponent;
-import de.innovationhub.prox.modules.commons.application.event.EventPublisher;
 import de.innovationhub.prox.modules.commons.application.usecase.UseCaseHandler;
 import de.innovationhub.prox.modules.profile.domain.user.User;
 import de.innovationhub.prox.modules.profile.domain.user.UserPort;
-import de.innovationhub.prox.modules.profile.domain.user.events.UserRegistered;
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
@@ -14,15 +12,12 @@ import lombok.RequiredArgsConstructor;
 public class RegisterUserUseCaseHandler implements UseCaseHandler<User, RegisterUser> {
 
   private final UserPort userPort;
-  private final EventPublisher eventPublisher;
 
   @Override
   @Transactional
   public User handle(RegisterUser useCase) {
-    var user = new User(useCase.id(), useCase.name(), useCase.email());
+    var user = User.register(useCase.id(), useCase.name(), useCase.email());
     user = userPort.save(user);
-
-    eventPublisher.publish(UserRegistered.from(user));
 
     return user;
   }

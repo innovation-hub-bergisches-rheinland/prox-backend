07f9aa596a53aa0588e802a4084224508a8a447e
Tobias Hund <tobias.hund@innovation-hub.de>
2022-10-19 13:45:42 +0200

    add feature for publishing collection of events

--- src/main/java/de/innovationhub/prox/modules/commons/application/event/EventPublisher.java
+++ src/main/java/de/innovationhub/prox/modules/commons/application/event/EventPublisher.java
@@ -1,5 +1,7 @@
 package de.innovationhub.prox.modules.commons.application.event;
 
+import java.util.Collection;
+
 /**
  * Event Publisher interface. Publishes events once they have been happened. Publishing can be done
  * synchronously or asynchronously using a in-process message bus or a remote message broker that
@@ -8,4 +10,8 @@ package de.innovationhub.prox.modules.commons.application.event;
 public interface EventPublisher {
 
   <EVENT extends Event> void publish(EVENT event);
+
+  default <EVENT extends Event> void publish(Collection<EVENT> events) {
+    events.forEach(this::publish);
+  }
 }

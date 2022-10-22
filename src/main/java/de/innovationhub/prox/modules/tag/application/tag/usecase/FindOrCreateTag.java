a5850c23c0f955478bffa055e5fba88088dbfe83
Tobias Hund <tobias.hund@innovation-hub.de>
2022-10-22 17:51:16 +0200

    remove obsolete usecase

--- src/main/java/de/innovationhub/prox/modules/tag/application/tag/usecase/FindOrCreateTag.java
+++
@@ -1,6 +0,0 @@
-package de.innovationhub.prox.modules.tag.application.tag.usecase;
-
-import de.innovationhub.prox.modules.commons.application.usecase.UseCase;
-
-public record FindOrCreateTag(String tag) implements UseCase {
-}

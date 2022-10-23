ac950ad6dec6a1fecaf26f3f657793d3412075ce
Tobias Hund <tobias.hund@innovation-hub.de>
2022-10-23 13:37:35 +0200

    rename projectport to repository

--- src/main/java/de/innovationhub/prox/modules/project/domain/project/ProjectRepository.java
+++ src/main/java/de/innovationhub/prox/modules/project/domain/project/ProjectRepository.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface ProjectPort {
+public interface ProjectRepository {
 
   Project save(Project project);
 

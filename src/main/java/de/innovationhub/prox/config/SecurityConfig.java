034ae26c7faefa9baa0c21a7ff922eab364374c2
Tobias Hund <tobias.hund@innovation-hub.de>
2022-11-07 18:57:15 +0100

    configure OAuth 2.0 resource server

--- src/main/java/de/innovationhub/prox/config/SecurityConfig.java
+++ src/main/java/de/innovationhub/prox/config/SecurityConfig.java
@@ -3,10 +3,18 @@ package de.innovationhub.prox.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 class SecurityConfig {
+
+  private JwtAuthenticationConverter jwtAuthenticationConverter() {
+    JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
+    jwtConverter.setJwtGrantedAuthoritiesConverter(new KeycloakGrantedAuthoritiesConverter());
+    jwtConverter.setPrincipalClaimName("sub");
+    return jwtConverter;
+  }
 
   @Bean
   public SecurityFilterChain configure(HttpSecurity http) throws Exception {
@@ -14,6 +22,8 @@ class SecurityConfig {
         .and()
         .csrf()
         .disable()
+        .oauth2ResourceServer(
+            oauth2 -> oauth2.jwt().jwtAuthenticationConverter(jwtAuthenticationConverter()))
         .authorizeHttpRequests(
             registry ->
                 registry

d69d159a1716a07864d94a67a48d4192545d2a71
Tobias Hund <tobias.hund@innovation-hub.de>
2022-11-09 16:33:33 +0100

    remove unused event/command abstraction

--- src/main/java/de/innovationhub/prox/modules/commons/application/command/IntegrationCommandHandler.java
+++
@@ -1,7 +0,0 @@
-package de.innovationhub.prox.modules.commons.application.command;
-
-import de.innovationhub.prox.modules.commons.contract.IntegrationCommand;
-
-public interface IntegrationCommandHandler<T extends IntegrationCommand> {
-  void handleCommand(T command);
-}
--- src/main/java/de/innovationhub/prox/modules/commons/application/event/IntegrationEventHandler.java
+++
@@ -1,7 +0,0 @@
-package de.innovationhub.prox.modules.commons.application.event;
-
-import de.innovationhub.prox.modules.commons.contract.IntegrationEvent;
-
-public interface IntegrationEventHandler <T extends IntegrationEvent> {
-  void handleEvent(T event);
-}

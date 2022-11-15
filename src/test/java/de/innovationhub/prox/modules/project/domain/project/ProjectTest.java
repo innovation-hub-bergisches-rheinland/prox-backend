3317ebb82b208d123e4577985a5a54e26acdd666
Tobias Hund <tobias.hund@innovation-hub.de>
2022-11-15 20:30:03 +0100

    add unit-tests for domain layer

--- src/test/java/de/innovationhub/prox/modules/project/domain/project/ProjectTest.java
+++ src/test/java/de/innovationhub/prox/modules/project/domain/project/ProjectTest.java
@@ -3,6 +3,14 @@ package de.innovationhub.prox.modules.project.domain.project;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import de.innovationhub.prox.modules.project.domain.project.events.ProjectArchived;
+import de.innovationhub.prox.modules.project.domain.project.events.ProjectCompleted;
+import de.innovationhub.prox.modules.project.domain.project.events.ProjectCreated;
+import de.innovationhub.prox.modules.project.domain.project.events.ProjectMarkedAsStale;
+import de.innovationhub.prox.modules.project.domain.project.events.ProjectOffered;
+import de.innovationhub.prox.modules.project.domain.project.events.ProjectStarted;
+import de.innovationhub.prox.modules.project.domain.project.events.ProjectTagged;
+import de.innovationhub.prox.modules.project.domain.project.events.ProjectUnarchived;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -14,6 +22,104 @@ import org.junit.jupiter.api.Test;
 class ProjectTest {
 
   @Test
+  void shouldRegisterCreateEvent() {
+    var project = Project.create(new Author(UUID.randomUUID()), null, "Test Project", "Test",
+        "Test", "Test", new CurriculumContext(), null);
+    assertThat(project.getDomainEvents())
+        .filteredOn(event -> event instanceof ProjectCreated)
+        .hasSize(1)
+        .first()
+        .isInstanceOfSatisfying(ProjectCreated.class, event -> {
+          assertThat(event.projectId()).isEqualTo(project.getId());
+        });
+  }
+
+  @Test
+  void shouldArchive() {
+    var project = createTestProject(ProjectState.PROPOSED);
+
+    project.archive();
+
+    assertThat(project.getStatus().getState())
+        .isEqualTo(ProjectState.ARCHIVED);
+    assertThat(project.getDomainEvents())
+        .filteredOn(event -> event instanceof ProjectArchived)
+        .hasSize(1)
+        .first()
+        .isInstanceOfSatisfying(ProjectArchived.class, event -> {
+          assertThat(event.projectId()).isEqualTo(project.getId());
+        });
+  }
+
+  @Test
+  void shouldUnarchive() {
+    var project = createTestProject(ProjectState.ARCHIVED);
+
+    project.unarchive();
+
+    assertThat(project.getStatus().getState())
+        .isEqualTo(ProjectState.PROPOSED);
+    assertThat(project.getDomainEvents())
+        .filteredOn(event -> event instanceof ProjectUnarchived)
+        .hasSize(1)
+        .first()
+        .isInstanceOfSatisfying(ProjectUnarchived.class, event -> {
+          assertThat(event.projectId()).isEqualTo(project.getId());
+        });
+  }
+
+  @Test
+  void shouldStale() {
+    var project = createTestProject(ProjectState.ARCHIVED);
+
+    project.stale();
+
+    assertThat(project.getStatus().getState())
+        .isEqualTo(ProjectState.STALE);
+    assertThat(project.getDomainEvents())
+        .filteredOn(event -> event instanceof ProjectMarkedAsStale)
+        .hasSize(1)
+        .first()
+        .isInstanceOfSatisfying(ProjectMarkedAsStale.class, event -> {
+          assertThat(event.projectId()).isEqualTo(project.getId());
+        });
+  }
+
+  @Test
+  void shouldStart() {
+    var project = createTestProject(ProjectState.OFFERED);
+
+    project.start();
+
+    assertThat(project.getStatus().getState())
+        .isEqualTo(ProjectState.RUNNING);
+    assertThat(project.getDomainEvents())
+        .filteredOn(event -> event instanceof ProjectStarted)
+        .hasSize(1)
+        .first()
+        .isInstanceOfSatisfying(ProjectStarted.class, event -> {
+          assertThat(event.projectId()).isEqualTo(project.getId());
+        });
+  }
+
+  @Test
+  void shouldEnd() {
+    var project = createTestProject(ProjectState.RUNNING);
+
+    project.complete();
+
+    assertThat(project.getStatus().getState())
+        .isEqualTo(ProjectState.COMPLETED);
+    assertThat(project.getDomainEvents())
+        .filteredOn(event -> event instanceof ProjectCompleted)
+        .hasSize(1)
+        .first()
+        .isInstanceOfSatisfying(ProjectCompleted.class, event -> {
+          assertThat(event.projectId()).isEqualTo(project.getId());
+        });
+  }
+
+  @Test
   void shouldAddSupervisorOnOffer() {
     var project = createTestProject(ProjectState.PROPOSED);
     var supervisor = new Supervisor(UUID.randomUUID());
@@ -21,6 +127,14 @@ class ProjectTest {
     project.offer(supervisor);
 
     assertThat(project.getSupervisors()).containsExactly(supervisor);
+    assertThat(project.getDomainEvents())
+        .filteredOn(event -> event instanceof ProjectOffered)
+        .hasSize(1)
+        .first()
+        .isInstanceOfSatisfying(ProjectOffered.class, event -> {
+          assertThat(event.projectId()).isEqualTo(project.getId());
+          assertThat(event.supervisor()).containsExactlyInAnyOrder(supervisor);
+        });
   }
 
   @Test
@@ -51,6 +165,24 @@ class ProjectTest {
     project.addSupervisor(supervisorHomer);
 
     assertThat(project.getSupervisors()).contains(supervisorHomer);
+  }
+
+  @Test
+  void shouldTagProject() {
+    var project = createTestProject(ProjectState.PROPOSED);
+    var tags = List.of(UUID.randomUUID(), UUID.randomUUID());
+
+    project.setTags(tags);
+
+    assertThat(project.getTags()).containsExactlyInAnyOrderElementsOf(tags);
+    assertThat(project.getDomainEvents())
+        .filteredOn(event -> event instanceof ProjectTagged)
+        .hasSize(1)
+        .first()
+        .isInstanceOfSatisfying(ProjectTagged.class, event -> {
+          assertThat(event.projectId()).isEqualTo(project.getId());
+          assertThat(event.tags()).containsExactlyInAnyOrderElementsOf(tags);
+        });
   }
 
   private Project createProjectWithSupervisors(Collection<Supervisor> supervisors) {

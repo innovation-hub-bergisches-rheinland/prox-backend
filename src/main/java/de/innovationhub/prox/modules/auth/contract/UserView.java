e7e86c00785b9c3922e00cb15d37f123eebb14d6
Tobias Hund <tobias.hund@innovation-hub.de>
2023-01-09 15:19:41 +0100

    mark views as serializable

--- src/main/java/de/innovationhub/prox/modules/auth/contract/UserView.java
+++ src/main/java/de/innovationhub/prox/modules/auth/contract/UserView.java
@@ -1,10 +1,11 @@
 package de.innovationhub.prox.modules.auth.contract;
 
+import java.io.Serializable;
 import java.util.UUID;
 
 public record UserView(
     UUID id,
     String name
-) {
+) implements Serializable {
 
 }
--- src/main/java/de/innovationhub/prox/modules/profile/contract/OrganizationView.java
+++ src/main/java/de/innovationhub/prox/modules/profile/contract/OrganizationView.java
@@ -1,10 +1,11 @@
 package de.innovationhub.prox.modules.profile.contract;
 
+import java.io.Serializable;
 import java.util.UUID;
 
 public record OrganizationView(
     UUID id,
     String name
-) {
+) implements Serializable {
 
 }

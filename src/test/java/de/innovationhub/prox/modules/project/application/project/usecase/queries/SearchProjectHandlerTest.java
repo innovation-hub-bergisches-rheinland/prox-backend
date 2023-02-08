3ce6e36bdd2d15ddc28178ba7399781f98f9b667
Tobias Hund <tobias.hund@innovation-hub.de>
2023-02-08 12:48:10 +0100

    add additional test

--- src/test/java/de/innovationhub/prox/modules/project/application/project/usecase/queries/SearchProjectHandlerTest.java
+++ src/test/java/de/innovationhub/prox/modules/project/application/project/usecase/queries/SearchProjectHandlerTest.java
@@ -12,6 +12,8 @@ import de.innovationhub.prox.modules.project.domain.project.ProjectRepository;
 import de.innovationhub.prox.modules.project.domain.project.ProjectState;
 import de.innovationhub.prox.modules.tag.contract.TagFacade;
 import de.innovationhub.prox.modules.tag.contract.TagView;
+import de.innovationhub.prox.modules.user.contract.profile.UserProfileFacade;
+import de.innovationhub.prox.modules.user.contract.profile.UserProfileView;
 import java.util.List;
 import java.util.UUID;
 import org.junit.jupiter.api.Test;
@@ -22,7 +24,8 @@ import org.springframework.data.domain.Pageable;
 class SearchProjectHandlerTest {
   ProjectRepository projectRepository = mock(ProjectRepository.class);
   TagFacade tagFacade = mock(TagFacade.class);
-  SearchProjectHandler searchProjectHandler = new SearchProjectHandler(projectRepository, tagFacade);
+  UserProfileFacade userProfileFacade = mock(UserProfileFacade.class);
+  SearchProjectHandler searchProjectHandler = new SearchProjectHandler(projectRepository, tagFacade, userProfileFacade);
 
   @Test
   void shouldCallRepository() {
@@ -34,7 +37,7 @@ class SearchProjectHandlerTest {
 
     searchProjectHandler.handle(status, keys, modules, text, null, page);
 
-    verify(projectRepository).filterProjects(eq(List.of("STALE")), eq(keys), eq(modules), eq(text), anyCollection(), eq(page));
+    verify(projectRepository).filterProjects(eq(List.of("STALE")), eq(keys), eq(modules), eq(text), anyCollection(), anyCollection(), eq(page));
   }
 
   @Test
@@ -47,7 +50,22 @@ class SearchProjectHandlerTest {
     searchProjectHandler.handle(null, null, null, null, List.of("tag1"), Pageable.unpaged());
 
     ArgumentCaptor<List<UUID>> captor = ArgumentCaptor.forClass((Class) List.class);
-    verify(projectRepository).filterProjects(any(), any(), any(), any() , captor.capture(), any());
+    verify(projectRepository).filterProjects(any(), any(), any(), any() , captor.capture(), anyCollection(), any());
     assertThat(captor.getValue()).containsExactly(givenTags.get(0).id());
+  }
+
+  @Test
+  void shouldCallRepositoryWithResolvedProfiles() {
+    var givenProfiles = List.of(
+        new UserProfileView(UUID.randomUUID(), "Xavier Tester")
+    );
+    var searchQuery = "test";
+    when(userProfileFacade.search(eq(searchQuery))).thenReturn(givenProfiles);
+
+    searchProjectHandler.handle(null, null, null, searchQuery, null, Pageable.unpaged());
+
+    ArgumentCaptor<List<UUID>> captor = ArgumentCaptor.forClass((Class) List.class);
+    verify(projectRepository).filterProjects(any(), any(), any(), any(), anyCollection(), captor.capture(), any());
+    assertThat(captor.getValue()).containsExactly(givenProfiles.get(0).id());
   }
 }

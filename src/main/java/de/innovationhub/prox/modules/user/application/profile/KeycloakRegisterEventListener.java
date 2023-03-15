dc890f01f87eea004c98df8523e9e04deef03339
Tobias Hund <tobias.hund@innovation-hub.de>
2023-03-15 10:29:56 +0100

    resolve user data from remote keycloak

--- src/main/java/de/innovationhub/prox/modules/user/application/profile/KeycloakRegisterEventListener.java
+++ src/main/java/de/innovationhub/prox/modules/user/application/profile/KeycloakRegisterEventListener.java
@@ -2,11 +2,13 @@ package de.innovationhub.prox.modules.user.application.profile;
 
 import de.innovationhub.prox.commons.stereotypes.ApplicationComponent;
 import de.innovationhub.prox.config.MessagingConfig;
+import de.innovationhub.prox.infra.keycloak.KeycloakClient;
 import de.innovationhub.prox.modules.user.application.profile.dto.CreateUserProfileRequestDto;
 import de.innovationhub.prox.modules.user.application.profile.dto.CreateUserProfileRequestDto.ContactInformationRequestDto;
 import de.innovationhub.prox.modules.user.application.profile.usecase.commands.CreateUserProfileHandler;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.keycloak.events.Event;
 import org.keycloak.events.EventType;
 import org.springframework.amqp.rabbit.annotation.Argument;
@@ -18,9 +20,11 @@ import org.springframework.messaging.handler.annotation.Payload;
 
 @ApplicationComponent
 @RequiredArgsConstructor
+@Slf4j
 public class KeycloakRegisterEventListener {
 
   private final CreateUserProfileHandler createUserProfile;
+  private final KeycloakClient keycloakClient;
 
   @RabbitListener(bindings = {
       @QueueBinding(
@@ -40,12 +44,34 @@ public class KeycloakRegisterEventListener {
 
     var firstName = event.getDetails().getOrDefault("first_name", "");
     var lastName = event.getDetails().getOrDefault("last_name", "");
-    // At the moment I don't feel very comfortable with the email as private information being
-    // persisted by default. We might want to change this in the future.
     var email = event.getDetails().getOrDefault("email", "");
-    var id = UUID.fromString(event.getUserId());
+    var strId = event.getUserId();
+    var id = UUID.fromString(strId);
 
-    var dto = new CreateUserProfileRequestDto(firstName + " " + lastName, "",
+    try {
+      // We try resolving user information from the remote Keycloak instance
+      // Reason for that is, that we get only valid event data (firstName/lastName) when the register
+      // form is being used. If the user uses a broker like Microsoft OIDC, we don't get the data.
+      // Hopefully the data is present in the remote Keycloak instance.
+      // TODO: We should probably rethink the whole event handling here. It's not really nice.
+
+      var userRepresentation = keycloakClient.getById(strId);
+      if (userRepresentation.isPresent()) {
+        var user = userRepresentation.get();
+        firstName = user.getFirstName();
+        lastName = user.getLastName();
+        email = user.getEmail();
+      } else {
+        log.warn(
+            "User with id {} registered, but it could not be found remotely. Using data from the event...",
+            strId);
+      }
+    } catch (Exception e) {
+      log.warn("User with id {} registered, but it could not be found remotely. Using data from the event...", strId, e);
+    }
+
+    var displayName = firstName + " " + lastName;
+    var dto = new CreateUserProfileRequestDto(displayName, "",
         new ContactInformationRequestDto(
             email,
             null,

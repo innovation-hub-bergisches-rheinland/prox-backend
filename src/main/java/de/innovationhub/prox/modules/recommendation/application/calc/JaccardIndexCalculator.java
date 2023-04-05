3679e67487d2bf1007115fc99f4d8df00edbf202
Tobias Hund <tobias.hund@innovation-hub.de>
2023-04-05 15:00:47 +0200

    add jaccard index

---
+++ src/main/java/de/innovationhub/prox/modules/recommendation/application/calc/JaccardIndexCalculator.java
@@ -0,0 +1,23 @@
+package de.innovationhub.prox.modules.recommendation.application.calc;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class JaccardIndexCalculator {
+
+  public <T> double calculate(Collection<T> setA, Collection<T> setB) {
+    if (setA.isEmpty() || setB.isEmpty()) {
+      return 0.0;
+    }
+
+    var sa = new HashSet<>(setA);
+    var sb = new HashSet<>(setB);
+
+    final var saSize = sa.size();
+    final var sbSize = sb.size();
+
+    sa.retainAll(sb);
+    final var intersection = sa.size();
+    return (double) intersection / (saSize + sbSize - intersection);
+  }
+}
--- src/main/java/de/innovationhub/prox/modules/recommendation/application/usecase/GetRecommendationsHandler.java
+++ src/main/java/de/innovationhub/prox/modules/recommendation/application/usecase/GetRecommendationsHandler.java
@@ -16,6 +16,10 @@ public class GetRecommendationsHandler {
     // 3.1. Get supervisors of those projects
     // 3.2. Get partners (organizations) of those projects
     // 4. Based on those results, calculate a confidence score for each supervisor, organization and project
+    // 4.1 The jaccard index can be used to calculate the confidence score for (1, 2, 3)
+    //     Another option is to simply count the number of matching tags
+    // 4.2 The confidence score for (3.1, 3.2) can be calculated based on the number of matching tags
+    // 4.3 We can then use both scores to calculate a final confidence score
     // 5. Return the top results for each category together with the confidence score
     return null;
   }

0b6fca3f1ad1c5edd9fd841d1ea346096758e8d3
Tobias Hund <tobias.hund@innovation-hub.de>
2023-05-31 09:57:50 +0200

    move things in methods

--- src/main/java/de/innovationhub/prox/modules/recommendation/application/usecase/GetRecommendationsHandler.java
+++ src/main/java/de/innovationhub/prox/modules/recommendation/application/usecase/GetRecommendationsHandler.java
@@ -3,6 +3,7 @@ package de.innovationhub.prox.modules.recommendation.application.usecase;
 import de.innovationhub.prox.commons.stereotypes.ApplicationComponent;
 import de.innovationhub.prox.config.CacheConfig;
 import de.innovationhub.prox.modules.organization.contract.OrganizationFacade;
+import de.innovationhub.prox.modules.organization.contract.dto.OrganizationDto;
 import de.innovationhub.prox.modules.project.contract.ProjectFacade;
 import de.innovationhub.prox.modules.project.contract.dto.ProjectDto;
 import de.innovationhub.prox.modules.project.domain.project.ProjectState;
@@ -14,11 +15,17 @@ import de.innovationhub.prox.modules.tag.contract.TagCollectionFacade;
 import de.innovationhub.prox.modules.tag.contract.dto.TagCollectionDto;
 import de.innovationhub.prox.modules.tag.contract.dto.TagDto;
 import de.innovationhub.prox.modules.user.contract.profile.UserProfileFacade;
+import de.innovationhub.prox.modules.user.contract.profile.dto.LecturerProfileDto;
+import de.innovationhub.prox.modules.user.contract.profile.dto.UserProfileDto;
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 import java.util.UUID;
 import java.util.function.Function;
 import java.util.stream.Stream;
@@ -60,48 +67,23 @@ public class GetRecommendationsHandler {
     // 4. Based on those results, calculate a confidence score for each supervisor, organization and project
     // 4.1 The overlap coefficent index can be used to calculate the confidence score for (1, 2, 3)
     //     Another option is to simply count the number of matching tags
-    final Map<UUID, Double> lecturerConfidenceScoreSum = new HashMap<>();
-    final Map<UUID, Double> organizationConfidenceScoreSum = new HashMap<>();
-    final Map<UUID, Double> projectConfidenceScoreSum = new HashMap<>();
+    final Map<UUID, Double> lecturerConfidenceScoreSum = calculateLecturerConfidenceScores(
+        matchingSupervisors, request.seedTags());
+    final Map<UUID, Double> organizationConfidenceScoreSum = calculateOrganizationConfidenceScores(
+        matchingOrganizations, request.seedTags());
+    final Map<UUID, Double> projectConfidenceScoreSum = calculateProjectConfidenceScores(
+        matchingProjects, request.seedTags());
 
-    for (var supervisor : matchingSupervisors) {
-      final var score = LECTURER_PROFILE_MATCH_BOOST_FACTOR * overlapCoefficientCalculator.calculate(request.seedTags(),
-          supervisor.tags().stream().map(
-              TagDto::id).toList());
-      addConfidenceScore(lecturerConfidenceScoreSum, supervisor.userId(), score);
-    }
-
-    for (var organization : matchingOrganizations) {
-      final var score = ORGANISATION_PROFILE_MATCH_BOOST_FACTOR * overlapCoefficientCalculator.calculate(request.seedTags(),
-          organization.tags().stream().map(
-              TagDto::id).toList());
-      addConfidenceScore(organizationConfidenceScoreSum, organization.id(), score);
-    }
-
-    for (var project : matchingProjects) {
-      final var projectScore = PROJECT_MATCH_BOOST_FACTOR * overlapCoefficientCalculator.calculate(request.seedTags(),
-          project.tags().stream().map(
-              TagDto::id).toList());
-      addConfidenceScore(projectConfidenceScoreSum, project.id(), projectScore);
-
-      var supervisors = project.supervisors();
-      if (projectScore > 0.0 && supervisors != null) {
-        for (var supervisor : supervisors) {
-          addConfidenceScore(lecturerConfidenceScoreSum, supervisor.id(), projectScore, 2.0);
-        }
-      }
-
-      var partner = project.partner();
-      if (projectScore > 0.0 && partner != null) {
-        addConfidenceScore(organizationConfidenceScoreSum, partner.id(), projectScore, 2.0);
-      }
-    }
+    // 4.2 The confidence score for (1, 2) can be boosted by matching projects
+    boostProjectParticipations(organizationConfidenceScoreSum, lecturerConfidenceScoreSum,
+        projectConfidenceScoreSum, matchingProjects);
 
     // 5. Return the top results for each category together with the confidence score
     final var topLecturers = pickResults(lecturerConfidenceScoreSum,
         e -> new RecommendationResponse.RecommendationResult<>(e.getValue(),
             userProfileFacade.getByUserId(e.getKey()).orElse(null)))
-        .filter(e -> e.item() != null && request.excludedIds().stream().noneMatch(ex -> e.item().userId().equals(ex)))
+        .filter(e -> e.item() != null && request.excludedIds().stream()
+            .noneMatch(ex -> e.item().userId().equals(ex)))
         .sorted((e1, e2) -> e2.confidenceScore().compareTo(e1.confidenceScore()))
         .limit(limit)
         .toList();
@@ -109,19 +91,23 @@ public class GetRecommendationsHandler {
     final var topOrganizations = pickResults(organizationConfidenceScoreSum,
         e -> new RecommendationResponse.RecommendationResult<>(e.getValue(),
             organizationFacade.get(e.getKey()).orElse(null)))
-        .filter(e -> e.item() != null && request.excludedIds().stream().noneMatch(ex -> e.item().id().equals(ex)))
+        .filter(e -> e.item() != null && request.excludedIds().stream()
+            .noneMatch(ex -> e.item().id().equals(ex)))
         .sorted((e1, e2) -> e2.confidenceScore().compareTo(e1.confidenceScore()))
         .limit(limit)
         .toList();
 
-    Comparator<RecommendationResult<ProjectDto>> projectComparator = Comparator.comparing(RecommendationResult::confidenceScore);
+    Comparator<RecommendationResult<ProjectDto>> projectComparator = Comparator.comparing(
+        RecommendationResult::confidenceScore);
     projectComparator = projectComparator.thenComparing(p -> p.item().createdAt()).reversed();
 
     final var topProjects = pickResults(projectConfidenceScoreSum,
         e -> new RecommendationResult<>(e.getValue(),
             projectFacade.get(e.getKey()).orElse(null)))
-        .filter(e -> e.item() != null && request.excludedIds().stream().noneMatch(ex -> e.item().id().equals(ex)))
-        .filter(e -> Arrays.stream(PROJECT_STATE_FILTER).anyMatch(s -> s.equals(e.item().status().state())))
+        .filter(e -> e.item() != null && request.excludedIds().stream()
+            .noneMatch(ex -> e.item().id().equals(ex)))
+        .filter(e -> Arrays.stream(PROJECT_STATE_FILTER)
+            .anyMatch(s -> s.equals(e.item().status().state())))
         .sorted(projectComparator)
         .limit(limit)
         .toList();
@@ -129,12 +115,86 @@ public class GetRecommendationsHandler {
     return new RecommendationResponse(topLecturers, topOrganizations, topProjects);
   }
 
-  private void addConfidenceScore(final Map<UUID, Double> map, final UUID id, final double score) {
-    addConfidenceScore(map, id, score, 1.0);
+  private void boostProjectParticipations(
+      Map<UUID, Double> organizationScores,
+      Map<UUID, Double> lecturerScores,
+      Map<UUID, Double> projectScores,
+      final Collection<ProjectDto> matchingProjects
+  ) {
+    for (var entry : projectScores.entrySet()) {
+      var project = matchingProjects.stream().filter(p -> p.id().equals(entry.getKey())).findFirst()
+          .orElse(null);
+      if (project == null) {
+        continue;
+      }
+
+      var score = entry.getValue();
+      var supervisors = project.supervisors();
+      if (score > 0.0 && supervisors != null) {
+        for (var supervisor : supervisors) {
+          addConfidenceScore(lecturerScores, supervisor.id(), score, 2.0);
+        }
+      }
+
+      var partner = project.partner();
+      if (score > 0.0 && partner != null) {
+        addConfidenceScore(organizationScores, partner.id(), score, 2.0);
+      }
+    }
   }
 
-  private void addConfidenceScore(final Map<UUID, Double> map, final UUID id, final double score, final double divisor) {
-    map.compute(id, (key, value) -> {
+  private Map<UUID, Double> calculateOrganizationConfidenceScores(
+      Collection<OrganizationDto> organizations, Collection<UUID> seedTags) {
+    final Map<UUID, Double> organizationConfidenceScoreSum = new HashMap<>();
+
+    for (var organization : organizations) {
+      final var score =
+          ORGANISATION_PROFILE_MATCH_BOOST_FACTOR * overlapCoefficientCalculator.calculate(seedTags,
+              organization.tags().stream().map(
+                  TagDto::id).toList());
+      addConfidenceScore(organizationConfidenceScoreSum, organization.id(), score);
+    }
+
+    return organizationConfidenceScoreSum;
+  }
+
+  private Map<UUID, Double> calculateLecturerConfidenceScores(Collection<UserProfileDto> lecturers,
+      Collection<UUID> seedTags) {
+    final Map<UUID, Double> lecturerConfidenceScoreSum = new HashMap<>();
+
+    for (var supervisor : lecturers) {
+      final var score =
+          LECTURER_PROFILE_MATCH_BOOST_FACTOR * overlapCoefficientCalculator.calculate(seedTags,
+              supervisor.tags().stream().map(
+                  TagDto::id).toList());
+      addConfidenceScore(lecturerConfidenceScoreSum, supervisor.userId(), score);
+    }
+
+    return lecturerConfidenceScoreSum;
+  }
+
+  private Map<UUID, Double> calculateProjectConfidenceScores(Collection<ProjectDto> projects,
+      Collection<UUID> seedTags) {
+    final Map<UUID, Double> projectConfidenceScoreSum = new HashMap<>();
+
+    for (var project : projects) {
+      final var projectScore =
+          PROJECT_MATCH_BOOST_FACTOR * overlapCoefficientCalculator.calculate(seedTags,
+              project.tags().stream().map(
+                  TagDto::id).toList());
+      addConfidenceScore(projectConfidenceScoreSum, project.id(), projectScore);
+    }
+
+    return projectConfidenceScoreSum;
+  }
+
+  private <T> void addConfidenceScore(final Map<T, Double> map, final T obj, final double score) {
+    addConfidenceScore(map, obj, score, 1.0);
+  }
+
+  private <T> void addConfidenceScore(final Map<T, Double> map, final T obj, final double score,
+      final double divisor) {
+    map.compute(obj, (key, value) -> {
       if (value == null) {
         return score;
       }

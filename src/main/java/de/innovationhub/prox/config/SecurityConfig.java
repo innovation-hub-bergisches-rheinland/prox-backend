0237b8a9b09046ca9f4e83d4b06207710c26cbf8
Tobias Hund <tobias.hund@innovation-hub.de>
2023-09-18 21:57:30 +0200

    fix exhaustiveness

--- src/main/java/de/innovationhub/prox/config/SecurityConfig.java
+++ src/main/java/de/innovationhub/prox/config/SecurityConfig.java
@@ -59,7 +59,7 @@ class SecurityConfig {
                     .permitAll()
                     .requestMatchers(HttpMethod.PUT, "/user/profile/lecturer")
                     .hasRole("professor")
-                    .requestMatchers("/user", "/user/tags", "/user/avatar")
+                    .requestMatchers("/user/profile", "/user/searchHistory", "/user/profile", "/user/profile/tags", "/user/profile/avatar")
                     .authenticated()
                     .requestMatchers(HttpMethod.GET, "/actuator/health/**")
                     .permitAll()

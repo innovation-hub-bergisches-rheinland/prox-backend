ab91d40124af66cae50857826038182c8d1179c7
Regrets <jannik-alx@gmx.de>
2023-12-21 16:04:57 +0100

    fix: find the users by their userId instead of their internal ID.

--- src/main/java/de/innovationhub/prox/modules/user/application/profile/usecase/queries/FindAllUserProfilesByUserIdsHandler.java
+++ src/main/java/de/innovationhub/prox/modules/user/application/profile/usecase/queries/FindAllUserProfilesByUserIdsHandler.java
@@ -3,6 +3,7 @@ package de.innovationhub.prox.modules.user.application.profile.usecase.queries;
 import de.innovationhub.prox.commons.stereotypes.ApplicationComponent;
 import de.innovationhub.prox.modules.user.domain.profile.UserProfile;
 import de.innovationhub.prox.modules.user.domain.profile.UserProfileRepository;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
@@ -10,13 +11,18 @@ import lombok.RequiredArgsConstructor;
 
 @ApplicationComponent
 @RequiredArgsConstructor
-public class FindAllUserProfilesByIdsHandler {
+public class FindAllUserProfilesByUserIdsHandler {
 
   private final UserProfileRepository userProfileRepository;
 
-  public List<UserProfile> handle(List<UUID> id) {
-    Objects.requireNonNull(id);
-
-    return userProfileRepository.findAllById(id);
+  public List<UserProfile> handle(List<UUID> userIds) {
+    Objects.requireNonNull(userIds);
+    var result = new ArrayList<UserProfile>();
+    for(UUID userId: userIds){
+      var found = userProfileRepository.findByUserId(userId);
+      found.ifPresent(result::add);
+    }
+    return result;
   }
+
 }
